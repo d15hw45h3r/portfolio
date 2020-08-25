da8a917c384861fe954720b20a1c14185bd2eeaf
d15hw45h3r <vinnichenkovaleriaa@gmail.com>
2020-08-25 20:05:11 +0300

    Added second page (empty), changed cv, minor changes

--- js/themes.js
+++ js/themes.js
@@ -5,6 +5,8 @@ let themeButtonEn = document.querySelector('.btn-en');
 themeButtonRu.onclick = function () {
   page.classList.toggle('light-theme');
   page.classList.toggle('dark-theme');
+  page.classList.toggle('work-light');
+  page.classList.toggle('work-dark');
 }
 
 themeButtonEn.onclick = function () {
