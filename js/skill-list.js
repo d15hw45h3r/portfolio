6718f243c477ab5ef663b999435f61e6f2d04415
d15hw45h3r <vinnichenkovaleriaa@gmail.com>
2020-10-02 17:59:24 +0300

    Remade skills section

---
+++ js/skill-list.js
@@ -0,0 +1,36 @@
+var skillDataHTML = [
+  "basics, semantics, layouts, positioning",
+  "display, box, grid, flex-box",
+  "responsive design",
+  "animation",
+  "svg pictures",
+  "emotion",
+];
+
+var skillDataJS = [
+  "syntax, basics",
+  "DOM manipulation",
+  "fetch API / Ajax",
+  "ES6 / modular JS",
+  "hooks",
+  "template manipulation",
+];
+
+var skillListHTML = document.querySelector(".skill-list-1");
+var skillListJS = document.querySelector(".skill-list-2");
+
+var addItem = function (item) {
+  var listItem = document.createElement("li");
+  listItem.textContent = item;
+  return listItem;
+};
+
+for (let i = 0; i < skillDataHTML.length; i++) {
+  let list = addItem(skillDataHTML[i]);
+  skillListHTML.appendChild(list);
+}
+
+for (let i = 0; i < skillDataJS.length; i++) {
+  let list = addItem(skillDataJS[i]);
+  skillListJS.appendChild(list);
+}
