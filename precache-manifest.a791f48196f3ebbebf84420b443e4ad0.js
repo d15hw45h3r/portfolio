c9b08c9ed2326b5bcc68a818c88acd92326fd8f6
Valeriia <43512665+d15hw45h3r@users.noreply.github.com>
2020-10-12 18:12:48 +0300

    Updates

---
+++ precache-manifest.a791f48196f3ebbebf84420b443e4ad0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa8eaf0558d7ad168e31820a9d620a47",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "dbff134d61cbd8349f53",
+    "url": "/portfolio/static/css/main.8fa4b4e9.chunk.css"
+  },
+  {
+    "revision": "b73cd50d97fd97842042",
+    "url": "/portfolio/static/js/2.4927dbab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.4927dbab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbff134d61cbd8349f53",
+    "url": "/portfolio/static/js/main.4978a2d5.chunk.js"
+  },
+  {
+    "revision": "e5e003f31d7a33643fc8",
+    "url": "/portfolio/static/js/runtime-main.6dafb600.js"
+  },
+  {
+    "revision": "d1faf80879fe566707d3cdcf94112b85",
+    "url": "/portfolio/static/media/cv.d1faf808.pdf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "57ac76048b31b8e631d93ac29e9aca91",
+    "url": "/portfolio/static/media/header-bg.57ac7604.jpg"
+  },
+  {
+    "revision": "f03eba2879bff0ec719675169927ed9e",
+    "url": "/portfolio/static/media/react-calculator.f03eba28.png"
+  },
+  {
+    "revision": "1c267f08b2272c82efbf5b2f01138ab9",
+    "url": "/portfolio/static/media/weather-project.1c267f08.png"
+  }
+]);
