5864ffdb2dfb775009fced689d44d336197de29e
Valeriia <43512665+d15hw45h3r@users.noreply.github.com>
2020-11-01 15:50:24 +0200

    changed image variables

--- src/components/main/projects/projects.js
+++ src/components/main/projects/projects.js
@@ -3,9 +3,9 @@ import "../../../App.css";
 
 import ProjectItem from "./project-component";
 
-import img from "../../../files/react-calculator.png";
-import img2 from "../../../files/weather-project.png";
-import img3 from "../../../files/todo-list.png";
+import imgCalculator from "../../../files/react-calculator.png";
+import imgWeather from "../../../files/weather-project.png";
+import imgTodoList from "../../../files/todo-list.png";
 
 const Projects = () => {
   return (
@@ -15,19 +15,19 @@ const Projects = () => {
           link="https://d15hw45h3r.github.io/calculator/"
           header="React calculator"
           description="This is my first project using React."
-          image={img}
+          image={imgCalculator}
         />
         <ProjectItem
           link="https://github.com/d15hw45h3r/react-weather"
           header="Weather app"
           description="A set of weather cards using OpenWeatherMap API."
-          image={img2}
+          image={imgWeather}
         />
         <ProjectItem
           link="https://d15hw45h3r.github.io/todo-list/"
           header="Todo List"
           description="This is a simple todo list created to practice useState."
-          image={img3}
+          image={imgTodoList}
         />
       </div>
     </section>
