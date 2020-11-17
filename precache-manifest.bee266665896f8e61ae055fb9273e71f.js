3ddc45784c2e618b986a7be26e6848a7c6fd38b6
Valeriia <43512665+d15hw45h3r@users.noreply.github.com>
2020-11-17 21:47:50 +0200

    Updates

--- precache-manifest.bee266665896f8e61ae055fb9273e71f.js
+++ precache-manifest.bee266665896f8e61ae055fb9273e71f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "612d0fcb5931ea991e735c99e431fc47",
+    "revision": "76faf3c0760aa87024bdeaab18805a79",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "2e19060932016d6540e5",
-    "url": "/portfolio/static/css/main.99d0bf8c.chunk.css"
+    "revision": "2a7e339713e461ef2f59",
+    "url": "/portfolio/static/css/main.a723dc50.chunk.css"
   },
   {
-    "revision": "751352507ffa90f88d3e",
-    "url": "/portfolio/static/js/2.20353868.chunk.js"
+    "revision": "5d9a61aa2f82be6424ee",
+    "url": "/portfolio/static/js/2.f1edd6a5.chunk.js"
   },
   {
     "revision": "0d6c4affe2a659999d44765f9a17cb4f",
-    "url": "/portfolio/static/js/2.20353868.chunk.js.LICENSE.txt"
+    "url": "/portfolio/static/js/2.f1edd6a5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2e19060932016d6540e5",
-    "url": "/portfolio/static/js/main.25e56d02.chunk.js"
+    "revision": "2a7e339713e461ef2f59",
+    "url": "/portfolio/static/js/main.be708548.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
@@ -34,10 +34,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "f03eba2879bff0ec719675169927ed9e",
     "url": "/portfolio/static/media/react-calculator.f03eba28.png"
-  },
-  {
-    "revision": "d58686a56fdf4082a87f887818a6b8be",
-    "url": "/portfolio/static/media/todo-list.d58686a5.png"
   },
   {
     "revision": "1c267f08b2272c82efbf5b2f01138ab9",
